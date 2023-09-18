12333591d9ea28ca91d0f19001648c3272157ed5
Sangita1511 <66407971+Sangita1511@users.noreply.github.com>
2023-09-18 11:08:22 +0530

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,46 @@
+package BookMS;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+
+public class Book {
+	 private String bookName;
+	    private int bookId;
+	    private String isbn;
+
+	 
+
+	    public Book(String bookName, int bookId, String isbn) {
+	        this.bookName = bookName;
+	        this.bookId = bookId;
+	        this.isbn = isbn;
+	    }
+
+	 
+
+	    public String getBookName() {
+	        return bookName;
+	    }
+
+	 
+
+	    public int getBookId() {
+	        return bookId;
+	    }
+
+	 
+
+	    public String getIsbn() {
+	        return isbn;
+	    }
+
+	 
+
+	    @Override
+	    public String toString() {
+	        return "Book ID: " + bookId + "\nBook Name: " + bookName + "\nISBN: " + isbn;
+	    }
+}
